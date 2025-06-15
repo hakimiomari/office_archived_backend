385a4ee96be7fdf8329b244dfd567fd65e7bd779
hakimiomari <hakimikamranullah@gmail.com>
2025-06-15 11:27:56 +0430

    make change in app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,16 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { AuthModule } from './auth/auth.module';
-import { PrismaService } from './prisma/prisma.service';
-import { PrismaModule } from './prisma/prisma.module';
-import { RedisModule } from './redis/redis.module';
-import { UserModule } from './user/user.module';
-import { ArchivesModule } from './archives/archives.module';
-import { ReportsService } from './reports/reports.service';
+import { Module } from "@nestjs/common";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+import { AuthModule } from "./auth/auth.module";
+import { PrismaService } from "./prisma/prisma.service";
+import { PrismaModule } from "./prisma/prisma.module";
+import { RedisModule } from "./redis/redis.module";
+import { UserModule } from "./user/user.module";
+import { ArchivesModule } from "./archives/archives.module";
 
 @Module({
   imports: [AuthModule, PrismaModule, RedisModule, UserModule, ArchivesModule],
   controllers: [AppController],
-  providers: [AppService, PrismaService, ReportsService],
+  providers: [AppService, PrismaService],
 })
 export class AppModule {}
